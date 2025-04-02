af78ccbbf3bfddfa7783143a2413599df0bf6bdb
alesstongwen <alesstongwen@gmail.com>
2025-04-02 15:46:02 -0700

    update on turso db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,12 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createClient } from "@libsql/client";
+
+export const workouts = sqliteTable("workouts", {
+	name: text("name").notNull(),
+	date: text("date").notNull()
+});
 
 export const db = createClient({
 	url: process.env.TURSO_DB_URL!,
-	authToken: process.env.TURSO_AUTH_TOKEN
+	authToken: process.env.TURSO_DB_AUTH_TOKEN!
 });
